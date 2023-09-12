59cc5b7f9f0abb6100a3761e9ddafbc5f5d752b3
goni <122239382+gonixxxx@users.noreply.github.com>
2023-09-12 23:09:51 +0900

    Time: 0 ms (100.00%), Space: 39.1 MB (55.73%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    static int[] arr;
+    public int climbStairs(int n) {
+        arr = new int[n+1];
+        arr[0] = arr[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        
+        return arr[n];
+    }
+}
